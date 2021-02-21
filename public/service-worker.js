8c6995dfd3d266775b5782afe5019c71b66913b8
francine babauta <francine.babauta@outlook.com>
2021-02-21 15:00:18 -0800

    added files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,14 @@
 // add files to cache
 const FILES_TO_CACHE = [
-     "/", 
-  "/index.html", 
-  "/index.js", 
-  "/favicon.ico", 
-  "/styles.css", 
-  "/icons/icon-192x192.png", 
-  "/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/assets/css/styles.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 const CACHE_NAME = "static-cache-v2";
