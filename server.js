96429a22f7bbd97f031136363d067e0915d19371
francine babauta <francine.babauta@outlook.com>
2021-02-21 13:50:00 -0800

    added process.env. to mongoose connect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const apiRoutes = require('./routes/api.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+);
 
 // routes
 app.use(require("./routes/api.js"));
