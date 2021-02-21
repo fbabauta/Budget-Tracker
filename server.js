0aec31a5d93f70f188365e175b147734768df92d
francine babauta <francine.babauta@outlook.com>
2021-02-21 13:59:19 -0800

    no major changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const apiRoutes = require('./routes/api.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,17 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget",
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-);
-
 // routes
 app.use(require("./routes/api.js"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
